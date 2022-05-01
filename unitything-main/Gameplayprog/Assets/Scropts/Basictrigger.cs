5417c9e978427297c5b666acf0b59d43db98c797
Theo <Theo2.Purssell@live.uwe.ac.uk>
2022-05-01 18:35:47 +0100

    Lake practically done, needs an enemy
    Scene 1 is basically complete at this point, just needs polish, enemies, and potentially some obstacles like spikes or swinging axes.

---
+++ unitything-main/Gameplayprog/Assets/Scropts/Basictrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basictrigger : MonoBehaviour
+{
+    public bool activated = false;
+    public bool buttonmode = false;
+    private bool buttonmodeactive = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!buttonmode)
+        {
+            activated = true;
+        }
+        else
+        {
+            buttonmodeactive = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        activated = false;
+        buttonmodeactive = false;
+    }
+    private void Update()
+    {
+        if (buttonmodeactive)
+        {
+            if (Input.GetButtonDown("Use"))
+            {
+                activated = true;
+            }
+        }
+    }
+}
