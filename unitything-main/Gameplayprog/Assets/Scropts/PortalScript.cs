0c3d67730b426e3821e9061e21faf9d0ec633943
LooVik <loovikwong@gmail.com>
2022-05-02 02:38:20 +0100

    Added portal system and code

---
+++ unitything-main/Gameplayprog/Assets/Scropts/PortalScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+  public GameObject Player;
+  [SerializeField] private PortalScript target;
+
+  [SerializeField] private Transform spawnPoint;
+  public Transform spawnP{get {return spawnPoint;}}
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if(other.gameObject.tag == "Player")
+    {
+      other.transform.position = target.spawnP.position;
+    }
+  }
+}
